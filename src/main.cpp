059aab5567fc233eb6a1a7f141a22c5c4cc9c6ee
Peter Bohm <peter@nextlogic.biz>
2021-11-09 10:56:59 +1000

    Implemented MQTT for observations and actions

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,10 @@
 #include <WiFi.h>
 #include <MQTT.h>
 
-const char ssid[] = "smc3";
+const char ssid[] = "smc6";
 const char pass[] = "nejaketazkeheslo";
-//const char mqt_ip[] = "192.168.15.127";
-const char mqt_ip[] = "192.168.86.202";
+const char mqt_ip[] = "192.168.15.127";
+//const char mqt_ip[] = "192.168.86.202";
 
 WiFiClient net;
 MQTTClient client;
@@ -27,72 +27,38 @@ bool autoSendData = false;
 
 int streaming_delay = 25;
 
+String observationsTopic = "pendulum-r/obs/r01";
+String actionsTopic = "pendulum-r/act/r01";
+
 void connect();
+void getBTData(String &topic, String &payload);
 
 void setup() {
+  Serial.begin(115200);
+  delay(2000);
+  Serial.println("Starting...");
+
   Wire.begin();
-  //Serial.begin(57600);
-  Serial2.begin(57600);
+  WiFi.begin(ssid, pass);
 
   ServoShield::init();
   BournsEncoder::init(10, 11, 12);
-}
 
-void getBTData() {
-  if (Serial2.available()) {
-    String cmd = Serial2.readStringUntil('\n');
-    char dir = cmd.charAt(0);
+  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported
+  // by Arduino. You need to set the IP address directly.
+  client.begin(mqt_ip, net);
+  client.onMessage(getBTData);
 
-    switch(dir) {
-      case 'a':
-        position_update = -ServoShield::FAST;
-        break;
-      case 's':
-        position_update = -ServoShield::SLOW;
-        break;
-      case 'd':
-        position_update = ServoShield::STOP;
-        break;
-      case 'f':
-        position_update = ServoShield::SLOW;
-        break;
-      case 'g':
-        position_update = ServoShield::FAST;
-        break;
-      case 'm':
-        position_update = cmd.substring(1).toInt();
-        break;
-      case 'o':
-        encoder_offset = atoi(cmd.substring(1).c_str());
-        break;
-      case 'u':
-        streaming_delay = atoi(cmd.substring(1).c_str());
-        break;
-      case 'z':
-        sendData = true;
-        break;
-      case 'x':
-        autoSendData = true;
-        break;
-      case 'y':
-        autoSendData = false;
-        break;
-
-//      case 'p':
-//        showText = cmd.substring(1);
-//        Lcd::print_short(showText);
-//        break;
-      default: break; // keep the current course
-    }
-    Serial2.flush();
-  }
+  connect();
 }
 
 void loop() {
+  client.loop();
+  delay(10);  // <- fixes some issues with WiFi stability
+
   unsigned long newMillis = millis();
 
-  // because the bluetooth module barfs on faster speeds
-  if (Serial2.availableForWrite() && (sendData || (autoSendData && (newMillis - dataSentTS > streaming_delay)))) {
+  if ((sendData || (autoSendData && (newMillis - dataSentTS > streaming_delay)))) {
     BournsEncoder::read();
 
     int time_delta = (int) (newMillis - lastMillis);
@@ -103,7 +69,7 @@ void loop() {
     lastPosition = position;
 
     char state[100] = ""; // without the initialization it contains new lines and unprintable chars
-    sprintf(state, "S%ld,%d,%d,%f,%f,%f\n",
+    sprintf(state, "S%ld,%d,%d,%f,%f,%f",
             newMillis,
             position,
             ServoShield::pulselen,
@@ -112,21 +78,14 @@ void loop() {
             velocity
     );
 
-    Serial2.print(state);
+     Serial.print(state);
+    client.publish(observationsTopic, state);
     dataSentTS = newMillis;
 
     if (sendData) {
       sendData = false;
     }
   }
-
-  getBTData();
-//  Serial.print("Received position update: ");
-//  Serial.println(position_update);
-
-//  Lcd::printTelemetry(distance, speed, EncoderE6B2::counter, VoltMeter::voltage);
-
-  delay(10);
 
   // throttle when omega is over 2 rotations per second
   if (fabs(BournsEncoder::omegas.average()) > 2) {
@@ -150,16 +109,55 @@ void connect() {
 
   Serial.println("\nconnected!");
 
-  client.subscribe("hello");
-  // client.unsubscribe("hello");
+  client.subscribe(actionsTopic);
 }
 
-void messageReceived(String &topic, String &payload) {
-  Serial.print(millis());
-  Serial.println(" incoming: " + topic + " - " + payload);
 
-  // Note: Do not use the client in the callback to publish, subscribe or
-  // unsubscribe as it may cause deadlocks when other things arrive while
-  // sending and receiving acknowledgments. Instead, change a global variable,
-  // or push to a queue and handle it in the loop after calling `client.loop()`.
+void getBTData(String &topic, String &payload) {
+  Serial.print("Received command: ");
+  Serial.println(payload);
+
+  char dir = payload.charAt(0);
+
+  switch(dir) {
+    case 'a':
+      position_update = -ServoShield::FAST;
+      break;
+    case 's':
+      position_update = -ServoShield::SLOW;
+      break;
+    case 'd':
+      position_update = ServoShield::STOP;
+      break;
+    case 'f':
+      position_update = ServoShield::SLOW;
+      break;
+    case 'g':
+      position_update = ServoShield::FAST;
+      break;
+    case 'm':
+      position_update = payload.substring(1).toInt();
+      break;
+    case 'o':
+      encoder_offset = atoi(payload.substring(1).c_str());
+      break;
+    case 'u':
+      streaming_delay = atoi(payload.substring(1).c_str());
+      break;
+    case 'z':
+      sendData = true;
+      break;
+    case 'x':
+      autoSendData = true;
+      break;
+    case 'y':
+      autoSendData = false;
+      break;
+
+//      case 'p':
+//        showText = cmd.substring(1);
+//        Lcd::print_short(showText);
+//        break;
+    default: break; // keep the current course
+  }
 }
